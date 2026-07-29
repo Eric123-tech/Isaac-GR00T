62060728d46f1fed80271eb78d5719b95cc083b3
Eric123-tech <2862135432@qq.com>
2026-07-29 10:27:43 -0700

    1

---
+++ examples/ARTIMANO/1292e_artimano_60fps_canonical62_native56_h16_hist1s_config.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES
+# SPDX-License-Identifier: Apache-2.0
+
+from gr00t.configs.data.embodiment_configs import register_modality_config
+from gr00t.data.embodiment_tags import EmbodimentTag
+from gr00t.data.types import ActionConfig, ActionFormat, ActionRepresentation, ActionType, ModalityConfig
+
+
+canonical62_native56_60fps_config = {
+    "video": ModalityConfig(
+        delta_indices=[-60, 0],
+        modality_keys=["ego_view"],
+    ),
+    "state": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=[
+            "left_wrist_pose",
+            "right_wrist_pose",
+            "left_hand_qpos",
+            "right_hand_qpos",
+        ],
+    ),
+    "action": ModalityConfig(
+        delta_indices=list(range(16)),
+        modality_keys=[
+            "right_root_raw_wrench",
+            "right_hand_raw_command",
+            "left_root_raw_wrench",
+            "left_hand_raw_command",
+        ],
+        action_configs=[
+            ActionConfig(ActionRepresentation.ABSOLUTE, ActionType.NON_EEF, ActionFormat.DEFAULT),
+            ActionConfig(ActionRepresentation.ABSOLUTE, ActionType.NON_EEF, ActionFormat.DEFAULT),
+            ActionConfig(ActionRepresentation.ABSOLUTE, ActionType.NON_EEF, ActionFormat.DEFAULT),
+            ActionConfig(ActionRepresentation.ABSOLUTE, ActionType.NON_EEF, ActionFormat.DEFAULT),
+        ],
+    ),
+    "language": ModalityConfig(
+        delta_indices=[0],
+        modality_keys=["annotation.human.action.task_description"],
+    ),
+}
+
+
+register_modality_config(
+    canonical62_native56_60fps_config,
+    embodiment_tag=EmbodimentTag.NEW_EMBODIMENT,
+)
